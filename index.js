293566f179afe659d5464c1fc17b6c982ab46f67
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2026-04-15 08:08:12 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// 3. Set up CORS (allow all origins for now)
+app.use(cors({
+  origin: '*'
+}));
+
+
+app.get('/', (req, res) => {
+  res.json({
+    message: "server is running",
+    status: "success"
+  });
+});
+
+// 5. Open the gates!
+app.listen(PORT, () => {
+  console.log(`Tower is alive at http://localhost:${PORT}`);
+});
